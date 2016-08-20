29a3be32f00c4c20c6e61c53fbf34c696d0b68a5
CAMILO <CAMILO@CAMILO>
2016-08-20 20:14:44 -0300

    Creación de primera versión de la interfaz de usuario

---
+++ CG.KarateScoreBoard/CG.KarateScoreBoard.UI.Desktop/NotifyPropertyChanged.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CG.KarateScoreBoard.UI.Desktop
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void OnPropertyChanged(String propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
